c71443c4cd17b4d100e7b5c418fcbcb4578271c0
unknown <shimantodevjs@gmail.com>
2023-07-08 19:31:11 +0300

    featuredphotos added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import SliderComp from '../../Components/Slider/Slider'
+import ImgSlider from '../../Components/Slider/imgSlider'
 import FeaturedProducts from '../../Components/FeaturedProducts/FeaturedProducts'
 import './Home.scss'
 const Home = () => {
+
   return (
     <div className='home'>
-        <SliderComp />
-        <FeaturedProducts type="featured" />
-        <FeaturedProducts type="trending" />
+        <ImgSlider />
+        <FeaturedProducts type="Featured" />
+        <FeaturedProducts type="Trending" />
     </div>
   )
 }
