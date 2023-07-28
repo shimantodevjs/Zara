55d0574547f4e8e6d182ce75c507e9ac2b6e9669
unknown <shimantodevjs@gmail.com>
2023-07-29 01:15:33 +0300

    minor bugs fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,43 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link, Outlet, useLocation } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Product from "./pages/Product/Product";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
-import './App.scss'
+import './App.scss';
 
+const Layout = () => {
+  const { pathname } = useLocation();
 
-const Layout=()=>{
-return(
-  <div className="app">
-    <Navbar />
-    <Outlet />
-    <Footer />
-  </div>
-  )
-}
-
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout/>,
-    children:[
-      {
-        path:'/',
-        element:<Home />
-      },
-      {
-        path:'/products/:id',
-        element:<Products/>
-      },
-      {
-        path:'/product/:id',
-        element:<Product/>
-      }
-    ]
-  }
-])
-
-function App() {
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
-    <div>
-       <RouterProvider router={router}/>
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="products/:id" element={<Products />} />
+            <Route path="product/:id" element={<Product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
--- client/src/Components/Categories/Categories.jsx
+++ client/src/Components/Categories/Categories.jsx
@@ -1,72 +1,72 @@
 import React from 'react'
 import './Categories.scss'
+import { Grid} from '@mui/material'
 import { Link } from 'react-router-dom'
 
 const Categories = () => {
   return (
     <div className='categories'>
-      <div className="wrapper">
-      <div className="col">
-        <div className="row">
-          <img src="https://images.unsplash.com/photo-1576827471288-0a8f6d6c937b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=80" alt="" />
+      <Grid container spacing={1} className='gridContainer'>
+
+      <Grid item xs={12} md={6} lg={3} className='gridItem' >
+          <img src="https://images.unsplash.com/photo-1529139574466-a303027c1d8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="" />
           <Link to='/products/1' className='link'>
           <button>
              sale
           </button>
           </Link>
-        </div>
-        <div className="row">
+      </Grid>
+
+      
+     
+      <Grid item xs={12} md={6} lg={6} className='gridItem'  >
+          <img src="https://images.unsplash.com/photo-1538329972958-465d6d2144ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
+          <Link to='/products/3' className='link'>
+          <button>
+             new season
+          </button>
+          </Link>
+      </Grid>
+       
+       <Grid item xs={12} md={6} lg={3} className='gridItem'>
+          <img src="https://images.unsplash.com/photo-1550896026-50bc5e53beda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="" />
+            <Link to='/products/4' className='link'>
+          <button>
+             men
+          </button>
+            </Link>
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={3} className='gridItem' >
           <img src="https://images.unsplash.com/photo-1592948078640-39656341be54?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
           <Link to='/products/2' className='link'>
           <button>
              women
           </button>
           </Link>
-        </div>
-      </div>
-      <div className="col">
-        <div className="row">
-          <img src="https://images.unsplash.com/photo-1529139574466-a303027c1d8b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="" />
-          <Link to='/products/3' className='link'>
-          <button>
-             new season
-          </button>
-          </Link>
-        </div>
-      </div>
-      <div className="col col-l">
-        <div className="row menAccRow">
-          <div className="col">
-            <div className="row"><img src="https://images.unsplash.com/photo-1550896026-50bc5e53beda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="" />
-            <Link to='/products/4' className='link'>
-          <button>
-             men
-          </button>
-            </Link>
-          </div>
-          </div>
-          <div className="col">
-            <div className="row ">
+      </Grid>
+
+        <Grid item xs={12} md={6} lg={3} className='gridItem'>
               <img src="https://images.unsplash.com/photo-1583292650898-7d22cd27ca6f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YWNjZXNzb3JpZXN8ZW58MHwxfDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
               <Link to='/products/5' className='link'>
               <button>
                  accessories
               </button>
               </Link>
-            </div>
-          </div>
-        </div>
+        </Grid>
+
+  
         
-        <div className="row row-shoe"><img src="https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXN8ZW58MHwwfDB8fHww&auto=format&fit=crop&w=500&q=60" alt="" />
+        <Grid item xs={12} md={6} lg={6} className='gridItem'>
+        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="" />
         <Link to='/products/6' className='link'>
           <button>
              shoes
           </button>
            </Link>
-          </div>
+          </Grid>
          
-      </div>
-      </div>
+      </Grid>
     </div>
   )
 }
